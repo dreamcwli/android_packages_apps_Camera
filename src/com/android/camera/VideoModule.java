de19fd56d88705b30c977bc1bde812b7569b76f5
Chia-Wei Li <dreamcwli@gmail.com>
2013-01-01 04:46:13 +0800

    Disable video snapshot if not supported by hardware
    Change-Id: Ib9129e68222155bf34a80996f2efa2e845cbda2d

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2480,6 +2480,10 @@ public class VideoModule implements CameraModule,
     // Preview area is touched. Take a picture.
     @Override
     public void onSingleTapUp(View view, int x, int y) {
+        if (!Util.isVideoSnapshotSupported(mParameters)) {
+            return;
+        }
+
         if (mMediaRecorderRecording && effectsActive()) {
             new RotateTextToast(mActivity, R.string.disable_video_snapshot_hint,
                     mOrientation).show();
